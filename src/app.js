9e096ab10166d00a8b387788aad81297620d7902
Faiyaz Rahman <faiyazrahman03@gmail.com>
2020-11-16 20:18:10 +0600

    get all the reffrence from index file to app.js

---
+++ src/app.js
@@ -0,0 +1,15 @@
+window.onload() = function(){
+    const inp = document.getElementById('inp');
+    const nameList = document.getElementById('name-list');
+    const display = document.getElementById('display');
+    const giveATry = document.getElementById('give-a-try');
+    const firstPosition = document.getElementById('first-position');
+    const secondPosition = document.getElementById('second-position');
+    const thirdPosition = document.getElementById('third-position');
+
+    //TODO: Extract Text from Text area and store into a array
+    //TODO: Render the names Extracted from text area
+    //TODO: Shuffle the names array to get random names
+    //TODO: Pick a random winner, Remove him/her name from the array
+    //TODO: Display winners name
+}
