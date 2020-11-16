c98d236f6e0e979d76cad18c28a639e90367975e
Faiyaz Rahman <faiyazrahman03@gmail.com>
2020-11-17 00:48:08 +0600

    modified split function

--- src/app.js
+++ src/app.js
@@ -17,93 +17,87 @@
     inp.addEventListener('keypress', function(event){
         if(event.key === 'Enter'){
             document.getElementById('button').click();
-            let newNames = event.target.value.split(', ');
-            if(newNames[0] !== ''){
-                newNames.forEach(name => {
-                    participantNames.push(name);
-                    let item = createListItem(name);
-                    nameList.appendChild(item);
-                });
-            }
+            let newNames = event.target.value.split();
+            console.log(newNames);
         }
     })
 
-    giveATry.addEventListener('click', function(){
-        if(participantNames.length === 0){
-            alert('There is no entry');
-        }else{
-            let shuffledNames = shuffle(participantNames);
-            for(let i = 1; i < shuffledNames.length; i++){
-                (function(i, count){
+    // giveATry.addEventListener('click', function(){
+    //     if(participantNames.length === 0){
+    //         alert('There is no entry');
+    //     }else{
+    //         let shuffledNames = shuffle(participantNames);
+    //         for(let i = 1; i < shuffledNames.length; i++){
+    //             (function(i, count){
                     
-                    setTimeout(() => {
-                        let rand = Math.floor(Math.random() * (shuffledNames.length));
-                        display.innerHTML = shuffledNames[rand];
+    //                 setTimeout(() => {
+    //                     let rand = Math.floor(Math.random() * (shuffledNames.length));
+    //                     display.innerHTML = shuffledNames[rand];
 
-                        if(count === shuffledNames.length - 1){
+    //                     if(count === shuffledNames.length - 1){
 
-                            if(!firstPosition.innerHTML){
-                                firstPosition.innerHTML = shuffledNames[rand];
-                                let index = participantNames.indexOf(shuffledNames[rand]);
-                                participantNames.splice(index, 1);
-                            }else if(!secondPosition.innerHTML){
-                                secondPosition.innerHTML = shuffledNames[rand];
-                                let index = participantNames.indexOf(shuffledNames[rand]);
-                                participantNames.splice(index, 1);
-                            }else if(!thirdPosition.innerHTML){
-                                thirdPosition.innerHTML = shuffledNames[rand];
-                                let index = participantNames.indexOf(shuffledNames[rand]);
-                                participantNames.splice(index, 1);
-                            }else {
-                                alert ('Draw is completed succesfully. Congratulations to the winners!');
-                            }
-                        }
+    //                         if(!firstPosition.innerHTML){
+    //                             firstPosition.innerHTML = shuffledNames[rand];
+    //                             let index = participantNames.indexOf(shuffledNames[rand]);
+    //                             participantNames.splice(index, 1);
+    //                         }else if(!secondPosition.innerHTML){
+    //                             secondPosition.innerHTML = shuffledNames[rand];
+    //                             let index = participantNames.indexOf(shuffledNames[rand]);
+    //                             participantNames.splice(index, 1);
+    //                         }else if(!thirdPosition.innerHTML){
+    //                             thirdPosition.innerHTML = shuffledNames[rand];
+    //                             let index = participantNames.indexOf(shuffledNames[rand]);
+    //                             participantNames.splice(index, 1);
+    //                         }else {
+    //                             alert ('Draw is completed succesfully. Congratulations to the winners!');
+    //                         }
+    //                     }
 
-                    }, i);
+    //                 }, i);
                     
-                    if(count === shuffledNames.length - 1){
+    //                 if(count === shuffledNames.length - 1){
 
-                        if(!firstPosition.innerHTML){
-                            firstPosition.innerHTML = shuffledNames[rand];
-                            let index = participantNames.indexOf(shuffledNames[rand]);
-                            participantNames.splice(index, 1);
-                        }else if(!secondPosition.innerHTML){
-                            secondPosition.innerHTML = shuffledNames[rand];
-                            let index = participantNames.indexOf(shuffledNames[rand]);
-                            participantNames.splice(index, 1);
-                        }else if(!thirdPosition.innerHTML){
-                            thirdPosition.innerHTML = shuffledNames[rand];
-                            let index = participantNames.indexOf(shuffledNames[rand]);
-                            participantNames.splice(index, 1);
-                        }else {
-                            alert ('Draw is completed succesfully. Congratulations to the winners!');
-                        }
-                    }
+    //                     if(!firstPosition.innerHTML){
+    //                         firstPosition.innerHTML = shuffledNames[rand];
+    //                         let index = participantNames.indexOf(shuffledNames[rand]);
+    //                         participantNames.splice(index, 1);
+    //                     }else if(!secondPosition.innerHTML){
+    //                         secondPosition.innerHTML = shuffledNames[rand];
+    //                         let index = participantNames.indexOf(shuffledNames[rand]);
+    //                         participantNames.splice(index, 1);
+    //                     }else if(!thirdPosition.innerHTML){
+    //                         thirdPosition.innerHTML = shuffledNames[rand];
+    //                         let index = participantNames.indexOf(shuffledNames[rand]);
+    //                         participantNames.splice(index, 1);
+    //                     }else {
+    //                         alert ('Draw is completed succesfully. Congratulations to the winners!');
+    //                     }
+    //                 }
 
-                })(i*100, i)
-            }
-        }
-    });
+    //             })(i*100, i)
+    //         }
+    //     }
+    // });
 
-    function createListItem(name){
-        let li = document.createElement('li');
-        li.className = 'list-group-item';
-        li.innerHTML = name;
-        return li;
-    }
+    // function createListItem(name){
+    //     let li = document.createElement('li');
+    //     li.className = 'list-group-item';
+    //     li.innerHTML = name;
+    //     return li;
+    // }
 
-    function shuffle(array) {
+    // function shuffle(array) {
 
-        let shuffledArray = [...array];
+    //     let shuffledArray = [...array];
 
-        for(let i = shuffledArray.length - 1; i > 0; i--){
-            let randomNumber = Math.floor(Math.random() * (i + 1));
-            let temporary = shuffledArray[randomNumber];
-            shuffledArray[randomNumber] = shuffledArray[i];
-            shuffledArray[i] = temporary;
-        }
-        return shuffledArray;
-    }
+    //     for(let i = shuffledArray.length - 1; i > 0; i--){
+    //         let randomNumber = Math.floor(Math.random() * (i + 1));
+    //         let temporary = shuffledArray[randomNumber];
+    //         shuffledArray[randomNumber] = shuffledArray[i];
+    //         shuffledArray[i] = temporary;
+    //     }
+    //     return shuffledArray;
+    // }
 
 
     
