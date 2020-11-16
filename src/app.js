5f4c809b07045ff0478e2dddb1c35fde9ba5db94
Faiyaz Rahman <faiyazrahman03@gmail.com>
2020-11-16 22:33:21 +0600

    fixed extract functionality

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+    //TODO: Extract Text from Text area and store into a array
+    //TODO: Render the names Extracted from text area
+    //TODO: Shuffle the names array to get random names
+    //TODO: Pick a random winner, Remove him/her name from the array
+    //TODO: Display winners name
 
     const inp = document.getElementById('inp');
     const nameList = document.getElementById('name-list');
@@ -13,15 +18,20 @@
         if(event.key === 'Enter'){
             let newNames = event.target.value.split(', ');
             if(newNames[0] !== ''){
-                newNames.foreEach(name => {
+                newNames.forEach(name => {
                     participantNames.push(name);
-                })
+                    let item = createListItem(name);
+                    nameList.appendChild(item);
+                });
             }
         }
     })
 
-    //TODO: Extract Text from Text area and store into a array
-    //TODO: Render the names Extracted from text area
-    //TODO: Shuffle the names array to get random names
-    //TODO: Pick a random winner, Remove him/her name from the array
-    //TODO: Display winners name
+    function createListItem(name){
+        let li = document.createElement('li');
+        li.className = 'list-group-item';
+        li.innerHTML = name;
+        return li;
+    }
+
+    
