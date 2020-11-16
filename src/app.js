ca965633aa5208b9589e54288a8fbdf785db1783
Faiyaz Rahman <faiyazrahman03@gmail.com>
2020-11-16 20:34:45 +0600

    extracted text from text area

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-window.onload() = function(){
+
     const inp = document.getElementById('inp');
     const nameList = document.getElementById('name-list');
     const display = document.getElementById('display');
@@ -7,9 +7,21 @@ window.onload() = function(){
     const secondPosition = document.getElementById('second-position');
     const thirdPosition = document.getElementById('third-position');
 
+    const participantNames = [];
+
+    inp.addEventListener('keypress', function(event){
+        if(event.key === 'Enter'){
+            let newNames = event.target.value.split(', ');
+            if(newNames[0] !== ''){
+                newNames.foreEach(name => {
+                    participantNames.push(name);
+                })
+            }
+        }
+    })
+
     //TODO: Extract Text from Text area and store into a array
     //TODO: Render the names Extracted from text area
     //TODO: Shuffle the names array to get random names
     //TODO: Pick a random winner, Remove him/her name from the array
     //TODO: Display winners name
-}
